956423e58685bb082c21f4c29ca49ba4b144a758
ARSHAD BAGWAN <70151782+aregularguy@users.noreply.github.com>
2022-12-07 10:00:58 +0530

    Update E6_frequent_itemset.py

--- E6_frequent_itemset.py
+++ E6_frequent_itemset.py
@@ -1,47 +1,52 @@
-import pandas
 import math
-from  itertools import combinations,permutations
+from itertools import combinations
 
 def read_data_in_dict(filename):
-   f = open(filename)
-   lines = f.readlines()
-   transactions = []
-   items = lines[0].split(',')
-   for line in lines[1:]:
-       transactions.append(list(map(int,line.split(','))))
-   data = {
-       'items' :items,
-       'transactions' : transactions
-   }  
-#    print('Transaction list is ')
-#    print(transactions)
-#    print('lines is ')
-#    print(items)  
-   return data
-data =read_data_in_dict('data.csv')   
+    f = open(filename)         
+    lines = f.readlines()
+    transactions = []
+    items = lines[0].split(',')
+    print(lines[2])
+    for line in lines[1:]:
+        transactions.append(list(map(int,line.split(','))))
+    data ={
+        'items':items,
+        'transactions':transactions
+    }
+    # print('Transaction list is ')
+    # print(transactions)
+    # print('lines is ')
+    # print(items)
+    return data
+data = read_data_in_dict('E:\lab-practice\data.csv')
 
 def get_freq(s,items,transactions):
-    freq_cnt = 0
+    freq_cnt=0
     for t in transactions:
-        tmp = 1
+        temp=1
         for item in s:
-            tmp *= t[items.index(item)]
-            if tmp ==1:
-                freq_cnt +=1
-    return freq_cnt         
-
-def frequent_itemset(data,level,min_support):
-    items = data["items"]
-    transactions = data["transactions"]
-    min_freq = math.ceil(min_support*len(transactions))
-    sets = list(combinations(items,level))
-    frequents_sets = []
-    for s in sets:
-        freq = get_freq(s,items,transactions)
-        if freq >= min_freq:
-            frequents_sets.append(s)
-    return frequents_sets        
-            
+            temp*=t[items.index(item)]
+        if temp==1:
+            freq_cnt += 1  
+    return freq_cnt
 
 
-print(frequent_itemset(data,2,0.5))
+def frequent_itemsets(data,level,min_support):
+        items = data['items']
+        transactions = data['transactions']
+        min_freq = math.ceil(min_support*len(transactions))
+        sets = list(combinations(items,level))
+        # print("combinations")
+        # print(sets)
+        # print("end of combinations")
+        frequent_sets = []
+        for s in sets:
+            freq=get_freq(s,items,transactions)
+            if freq>=min_freq:
+                frequent_sets.append(s)
+                # a,b,c,d,e,f,g
+        return frequent_sets
+
+
+
+print(frequent_itemsets(data,2,0.5))    
